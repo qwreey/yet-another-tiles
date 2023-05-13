26431be4e1d56a6ffdc14f032efef5752a2b974e
Qwreey <dev@qwreey.kr>
2023-05-14 06:03:03 +0900

    Resolve conflict

--- src/windowMover.js
+++ src/windowMover.js
@@ -1,23 +1,16 @@
 
 const { Meta, Clutter, Gio } = imports.gi
+const RENDER_DELAY = 3+1 // ignore initial call / first frame call (on resized) / after call (window redraw) + time to render window
 
 var WindowMover = class WindowMover {
   constructor() {
     this._windowAnimations = []
-    this._desktopSettings = new Gio.Settings({ schema: 'org.gnome.desktop.interface' })
   }
 
+  // deinit all animations
   destroy() {
-    this._windowAnimations.forEach(animation=>{
-      animation.clone?.destroy()
-      const actor = animation.actor
-      if (actor) {
-        actor.timeline?.run_dispose()
-        actor.timeline = null
-      }
-    })
-    this._desktopSettings.run_dispose()
-    this._desktopSettings = this._windowAnimations = null
+    this._windowAnimations.forEach(animation=>this._destroyAnimation(animation))
+    this._windowAnimations = null
   }
 
   // capture window content and create clone clutter
@@ -31,75 +24,68 @@ var WindowMover = class WindowMover {
     })
   }
 
-  // unmaximize without animation by hacking gsettings
-  _unmaximizeWithoutAnimation(window,rect) {
-    rect ??= window.get_frame_rect()
-    const lastValue = this._desktopSettings.get_boolean("enable-animations")
-    if (lastValue) this._desktopSettings.set_boolean("enable-animations",false)
-    window.unmaximize(Meta.MaximizeFlags.BOTH)
-    window.move_resize_frame(false, rect.x, rect.y, rect.width, rect.height)
-    if (lastValue) this._desktopSettings.set_boolean("enable-animations",true)
-  }
-
   // give time to redraw it selfs to application
-  _delayFrames(actor) {
+  // If canceled, return true
+  _delayFrames(actor,animation) {
     return new Promise(resolve=>{
-      const timeline = actor.timeline = new Clutter.Timeline({ actor:actor,duration: 10000 })
+      const timeline = animation.timeline = new Clutter.Timeline({ actor:actor,duration: 1000 })
       let count = 0
-      timeline.connect("new-frame",()=>{
-        if (++count<=5) return
+      animation.resolve = resolve
+      animation.newframe = timeline.connect("new-frame",()=>{
+        if (++count < RENDER_DELAY) return 
+        timeline.disconnect(animation.newframe)
         timeline.run_dispose()
-        actor.timeline = null
+        animation.resolve = animation.newframe = animation.timeline = null
         resolve()
       })
       timeline.start()
     })
   }
 
-  _lerp(source,target,progress) {
-    if (progress == 1) return target
-    else if (progress == 0) return source
-    return source + (target-source)*progress
-  }
+  // destroy last animation, Also cancel delayFraems
+  _destroyAnimation(animation,keepTransitions) {
+    const actor = animation.actor
 
-  _destroyAnimation(animation) {
-    animation?.clone?.destroy()
-    if (animation.timer) {
-      if (animation.newFrameEvent) animation.timer.disconnect(animation.newFrameEvent)
-      if (animation.completedEvent) animation.timer.disconnect(animation.completedEvent)
-    }
-    animation.timer.run_dispose()
+    // remove animation from lists
     const index = this._windowAnimations.indexOf(animation)
     if (index != -1) this._windowAnimations.splice(index,1)
+    
+    // kill transitions
+    if (!keepTransitions) {
+      animation?.clone?.remove_all_transitions()
+      animation?.clone?.destroy()
+      if (actor) {
+        actor.actor?.remove_all_transitions()
+        actor.scale_x = 1
+        actor.scale_y = 1
+        actor.translation_x = 0
+        actor.translation_y = 0
+      }
+      animation.clone = animation.actor = animation.window = null
+    }
+
+    // kill last delay
+    const timeline = animation?.timeline
+    if (timeline) {
+      timeline.disconnect(animation.newframe)
+      timeline.run_dispose()
+      const resolve = animation.resolve
+      actor.thaw()
+      animation.resolve = animation.newframe = animation.timeline = null
+      resolve(true)
+    }
   }
 
   async _setWindowRect(window, x, y, width, height, animate) {
-    const innerRectBefore = window.get_frame_rect()
-    const outterRectBefore = window.get_buffer_rect()
     const actor = window.get_compositor_private()
     const isMaximized = window.get_maximized()
     const lastAnimation = this._windowAnimations.find(item=>item.window === window)
     const thisAnimation = {}
     let clone
 
-    // unmaximize / reset all animations
-    if (isMaximized) {
-      clone = animate && this._captureWindow(actor,outterRectBefore)
-      this._unmaximizeWithoutAnimation(window,innerRectBefore)
-    }
-    if (lastAnimation) this._destroyAnimation(lastAnimation)
-
-    // clone window, and resize meta window
-    thisAnimation.clone = animate && (clone ??= this._captureWindow(actor,outterRectBefore))
-    thisAnimation.window = window
-    thisAnimation.actor = actor
-    window.move_resize_frame(false, x, y, width, height)
-    if (!animate) {
-      return
-    }
-    this._windowAnimations.push(thisAnimation)
-
     // Calculate before size / position
+    const innerRectBefore = window.get_frame_rect()
+    const outterRectBefore = window.get_buffer_rect()
     const cloneGoalScaleX = width/innerRectBefore.width
     const cloneGoalScaleY = height/innerRectBefore.height
     const actorInitScaleX = innerRectBefore.width/width
@@ -107,79 +93,70 @@ var WindowMover = class WindowMover {
     const decoLeftBefore  = (innerRectBefore.x-outterRectBefore.x)
     const decoTopBefore   = (innerRectBefore.y-outterRectBefore.y)
 
-    // draw clone, and wait for real window finish drawn
-    global.window_group.insert_child_above(clone,actor)
-    actor.visible = false
-    await this._delayFrames(actor)
-    if (this._windowAnimations.find(item=>item.window === window).clone != clone) {
-      clone.destroy()
+    // destroy last animation and freeze actor
+    if (lastAnimation) this._destroyAnimation(lastAnimation,animate) // destroy old animation (but keep keep transitions for smoother)
+    actor.freeze() // do not render while real resizing done
+
+    // unmaximize
+    if (isMaximized) {
+      // clone actor before unmaximize for animate maxed -> tiled
+      clone = animate && this._captureWindow(actor,outterRectBefore)
+      window.unmaximize(Meta.MaximizeFlags.BOTH)
+      actor.remove_all_transitions() // remove unmaximize animation
+    }
+
+    // save this animation / clone window
+    if (animate) {
+      thisAnimation.clone = clone ??= this._captureWindow(actor,outterRectBefore)
+      thisAnimation.window = window
+      thisAnimation.actor = actor
+      this._windowAnimations.push(thisAnimation)
+    }
+
+    // resize meta window / wait for window ready
+    window.move_resize_frame(false, x, y, width, height)
+    if (!animate) { // if no animate
+      actor.thaw() // allow render window
       return
     }
-    actor.visible = true
-
-    // Recalculate after size / position (required for real window)
-    let innerRectAfter = window.get_frame_rect()
-    let outterRectAfter = window.get_buffer_rect()
-    let decoLeftAfter  = (innerRectAfter.x-outterRectAfter.x)
-    let decoTopAfter   = (innerRectAfter.y-outterRectAfter.y)
+    const resultDelay = await this._delayFrames(actor,thisAnimation) // wait once for window size updating
+    if (lastAnimation) this._destroyAnimation(lastAnimation) // remove old transitions
+    if (resultDelay) return // If canceled, just return
+    global.window_group.insert_child_above(clone,actor) // insert clone on screen
 
     // Set real window actor position
     actor.scale_x = actorInitScaleX
     actor.scale_y = actorInitScaleY
-    actor.x = innerRectBefore.x - decoLeftAfter*actorInitScaleX
-    actor.y = innerRectBefore.y - decoTopAfter*actorInitScaleY
+    actor.translation_x = innerRectBefore.x - x
+    actor.translation_y = innerRectBefore.y - y
+    actor.thaw() // allow render window
 
     // Clone animation
     clone.ease_property('opacity', 0, {
       duration: 300,
       mode: Clutter.AnimationMode.EASE_OUT_QUART
     })
-    for (const prop of [
-      [clone,'scale_x',cloneGoalScaleX],
-      [clone,'scale_y',cloneGoalScaleY],
-      [clone,'x',x-decoLeftBefore*cloneGoalScaleX],
-      [clone,'y',y-decoTopBefore*cloneGoalScaleY]
-    ]) {
-      prop[0].ease_property(prop[1],prop[2],{
-        duration: 300,
-        mode: Clutter.AnimationMode.EASE_OUT_EXPO
-      })
-    }
+    clone.ease({
+      scale_x: cloneGoalScaleX,
+      scale_y: cloneGoalScaleY,
+      x: x-decoLeftBefore*cloneGoalScaleX,
+      y: y-decoTopBefore*cloneGoalScaleY,
+      duration: 300,
+      mode: Clutter.AnimationMode.EASE_OUT_EXPO,
+    })
 
     // Real window animation
-    const timer = thisAnimation.timer = new Clutter.Timeline({
-      actor: actor,
+    actor.ease({
+      scale_x: 1,
+      scale_y: 1,
+      translation_x: 0,
+      translation_y: 0,
       duration: 300,
-      progress_mode: Clutter.AnimationMode.EASE_OUT_EXPO,
+      mode: Clutter.AnimationMode.EASE_OUT_EXPO,
+      onStopped: ()=>{
+        const nowAnimation = this._windowAnimations.find(item=>item.window === window)
+        if (nowAnimation?.clone === clone) this._destroyAnimation(nowAnimation)
+      }
     })
-    thisAnimation.newFrameEvent = timer.connect('new-frame', ()=>{
-      const progress = timer.get_progress()
-      outterRectAfter = window.get_buffer_rect()
-      decoLeftAfter  = (innerRectAfter.x-outterRectAfter.x)
-      decoTopAfter   = (innerRectAfter.y-outterRectAfter.y)
-
-      actor.x = this._lerp(
-        innerRectBefore.x - decoLeftAfter*actorInitScaleX,
-        x-decoLeftAfter,
-        progress
-      )
-      actor.y = this._lerp(
-        innerRectBefore.y - decoTopAfter*actorInitScaleY,
-        y-decoTopAfter,
-        progress
-      )
-      actor.scale_x = this._lerp(actorInitScaleX,1,progress)
-      actor.scale_y = this._lerp(actorInitScaleY,1,progress)
-    })
-    thisAnimation.completedEvent = timer.connect('completed', ()=>{
-      outterRectAfter = window.get_buffer_rect()
-      actor.x = outterRectAfter.x
-      actor.y = outterRectAfter.y
-      actor.scale_y = actor.scale_x = 1
-
-      const nowAnimation = this._windowAnimations.find(item=>item.window === window)
-      if (nowAnimation && nowAnimation.clone === clone) this._destroyAnimation(nowAnimation)
-    })
-    timer.start()
   }
 }
